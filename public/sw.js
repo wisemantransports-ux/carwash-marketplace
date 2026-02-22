f28ea8647bff1e3e2c2eae5e08a0cb6835466bcd
Wiseman Transports <wisemantransports@gmail.com>
2026-02-22 15:39:33 +0000

    make the project pwa, anybody that visited on thier phones will be ask t

---
+++ public/sw.js
@@ -0,0 +1,23 @@
+// Simple service worker for PWA functionality
+const CACHE_NAME = 'carwash-cm-v1';
+const ASSETS = [
+  '/',
+  '/icon.svg',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
