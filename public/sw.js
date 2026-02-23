77f999bd95946626062472b787ea567dd190995d
Wiseman Transports <wisemantransports@gmail.com>
2026-02-23 20:14:20 +0000

    Try fixing this error: `Runtime Error: Failed to load chunk /_next/stati

--- public/sw.js
+++ public/sw.js
@@ -1,15 +1,11 @@
-// Basic Service Worker for Carwash Marketplace PWA
-const CACHE_NAME = 'cwm-cache-v1';
-const ASSETS_TO_CACHE = [
-  '/',
-  '/icon.svg',
-  '/manifest.json'
-];
-
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS_TO_CACHE);
+    caches.open('cwm-v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/icon.svg',
+        '/manifest.json'
+      ]);
     })
   );
 });
